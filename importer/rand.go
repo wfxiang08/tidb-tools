7876072477124d8ebba035a40171e9b2c6011523
choleraehyq <choleraehyq@gmail.com>
2016-09-30 11:37:12 +0800

    [*] Optimize randString
    Signed-off-by: choleraehyq <choleraehyq@gmail.com>

--- importer/rand.go
+++ importer/rand.go
@@ -26,6 +26,11 @@ const (
 	dateFormat     = "2006-01-02"
 	timeFormat     = "15:04:05"
 	dateTimeFormat = "2006-01-02 15:04:05"
+
+	// Used by randString
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
 func init() {
@@ -52,12 +57,23 @@ func randBool() bool {
 	return value == 1
 }
 
+// reference: http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 func randString(n int) string {
-	bytes := make([]byte, n)
-	for i := range bytes {
-		bytes[i] = alphabet[randInt(0, len(alphabet)-1)]
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(alphabet) {
+			b[i] = alphabet[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
 	}
-	return string(bytes)
+
+	return string(b)
 }
 
 func randDuration(n time.Duration) time.Duration {
