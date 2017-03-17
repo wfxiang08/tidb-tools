d7c1694419f9785ca4af7b1e7bcc466de57a6ed2
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-03-17 20:35:52 +0800

    fix retry bug (#102)

--- loader/db.go
+++ loader/db.go
@@ -68,7 +68,7 @@ func executeSQL(conn *Conn, sqls []string, enableRetry bool, skipConstraintCheck
 		if i > 0 {
 			skipConstraintCheck = false
 		}
-		if err = executeSQLImp(conn.db, sqls, skipConstraintCheck); !isErrDupEntry(err) {
+		if err = executeSQLImp(conn.db, sqls, skipConstraintCheck); err != nil && !isErrDupEntry(err) {
 			continue
 		}
 
